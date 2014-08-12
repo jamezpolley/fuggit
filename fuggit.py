527694fa063759112dd72c02f98d2c6689e8c935
James Polley <jp@jamezpolley.com>
2014-08-12 18:34:59 +1000

    Add add subcommand

--- fuggit.py
+++ fuggit.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import errno
+import logging
 import os
 import os.path
 
-from fabric.api import lcd, local, warn_only
+from fabric.api import local, warn_only
 import begin
 
 
@@ -17,18 +19,54 @@ def get_top_dir(path):
 
 
 @begin.subcommand
+def add(local_filename):
+    """Adds LOCAL_FILENAME from the remote system.
+
+    LOCAL_FILENAME has the format: remote.hostname/path/to/remote/file
+
+    This will pull /path/to/remote/file from the host named remoted.hostname
+    and place it in remote.hostname/path/to/remote/file under $PWD
+
+    The file is then added to git and committed.
+
+    LOCAL_FILENAME must not yet exist."""
+
+    if os.path.exists(local_filename):
+        raise OSError(errno.EEXIST, "%s already exists" % local_filename)
+    logging.debug("local: %s", local_filename)
+    hostname, remote_path = get_top_dir(local_filename)
+    logging.debug("hn: %s remote: %s", hostname, remote_path)
+    local("scp %s:%s %s" % (hostname, remote_path, local_filename))
+    with warn_only():
+        local("git add %s" % local_filename)
+        local("git commit -m 'Added %s from remote' %s" % (local_filename,
+                                                           local_filename))
+
+
+@begin.subcommand
 def pull(local_filename):
     """Pulls the remote copy of LOCAL_FILENAME"""
+    logging.debug("local: %s", local_filename)
     hostname, remote_path = get_top_dir(local_filename)
-    basedir = local("git root", capture=True)
-    with lcd(basedir):
-        local("git stash")
-        local("scp %s:%s %s" % (hostname, remote_path, local_filename))
-        with warn_only():
-            local("git commit -m 'Pull from remote' %s" % local_filename)
-            local("git stash pop")
+    logging.debug("hn: %s remote: %s", hostname, remote_path)
+    local("git stash")
+    local("scp %s:%s %s" % (hostname, remote_path, local_filename))
+    with warn_only():
+        local("git commit -m 'Pull from remote' %s" % local_filename)
+        local("git stash pop")
+
+
+@begin.subcommand
+def vimdiff(local_filename):
+    """Opens vimdiff on LOCAL_FILENAME and its remote counterpart"""
+    hostname, remote_path = get_top_dir(local_filename)
+    local("vimdiff %s scp://%s/%s" % (local_filename, hostname, remote_path))
 
 
 @begin.start
+@begin.logging
 def run():
+    """Fuggit: for when you want to put that file in git, but you can't be f
+
+    You know the feeling. You're looking for a"""
     pass
